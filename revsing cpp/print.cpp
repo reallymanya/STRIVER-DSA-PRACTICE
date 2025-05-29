0c39d15bc92c6b2fc2af7831d225b80702c8091a
reallymanya <manyatakkar.01@gmail.com>
2025-05-29 20:35:58 +0530

    pattern problems

---
+++ revsing cpp/print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void printNumber() {
+        int num;
+        cin >> num;
+        cout << num;
+    }
+};
+
+int main() {
+    Solution sol;
+    sol.printNumber();
+    return 0;
+}
