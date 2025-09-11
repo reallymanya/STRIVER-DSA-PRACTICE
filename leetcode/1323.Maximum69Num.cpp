a4f4d74198090a83c2f2e3a32847a278670312e4
reallymanya <manyatakkar.01@gmail.com>
2025-09-11 15:36:38 +0530

    easy leetcode

---
+++ leetcode/1323.Maximum69Num.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int maximum69Number (int num) {
+        string s = to_string(num);
+        for(int i = 0; i<s.length();i++){
+            if(s[i] == '6'){
+                s[i] = '9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+    
+int main(){
+    int n = 9669;
+    cout <<  maximum69Number(n);
+}
