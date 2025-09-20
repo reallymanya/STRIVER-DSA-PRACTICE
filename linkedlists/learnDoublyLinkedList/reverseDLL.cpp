9f3714ac123b776f0614db548be6ae5ec8cbaf24
reallymanya <manyatakkar.01@gmail.com>
2025-09-20 16:28:18 +0530

    reverse dll

---
+++ linkedlists/learnDoublyLinkedList/reverseDLL.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+
+    public:
+    Node(int data1,Node* next1,Node* back1){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+
+Node* convertArr2DLL(vector<int> &arr){
+Node* head = new Node(arr[0]);
+Node* prev = head;
+for(int i = 1; i<arr.size(); i++){
+    Node* temp = new Node(arr[i],nullptr,prev);
+    prev->next = temp;
+    prev = temp;
+}
+return head;
+}
+
+void print(Node* head){
+    while(head != NULL){
+        cout << head->data<<" ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node* reversell(Node* head){
+    if(head == NULL || head->next == NULL) return head;
+    Node* current = head;
+    Node* prev = NULL;
+
+    while(current != NULL){
+        prev = current ->back;
+        current->back = current->next;
+        current->next = prev;
+
+        current = current->back;
+    }
+    return prev->back;
+}
+
+int main(){
+    vector<int> arr = {2,5,8,7};
+    Node* head = convertArr2DLL(arr);
+    print(head);
+
+    head = reversell(head);
+    print(head);
+}
